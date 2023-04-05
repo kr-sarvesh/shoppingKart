4379aa30408d1650abf5f314262fd1e711f7a636
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-04-05 13:13:50 +0530

    added styles of addding and deleting items

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
-import { Card } from 'react-bootstrap'
+import { Card, Button } from 'react-bootstrap'
+import { formatCurrency } from '../utilities/formatCurrency'
 
 type StoreItemProps = {
   id: number
@@ -8,6 +9,7 @@ type StoreItemProps = {
 }
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const quantity = 1
   return (
     <Card>
       <Card.Img
@@ -16,8 +18,35 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         height='200px'
         style={{ objectFit: 'cover' }}
       />
-      <Card.Body className='d-flex flex-column justify-content-between'>
-        <Card.Title>{name}</Card.Title>
+      <Card.Body className='d-flex flex-column'>
+        <Card.Title className='d-flex justify-content-between align-items-baseline mb-4'>
+          <span className='fs-2'>{name}</span>
+          <span className='ms-2 text-muted'>{formatCurrency(price)}</span>
+        </Card.Title>
+        <div className='mt-auto'>
+          {quantity === 0 ? (
+            <Button className='btn btn-primary w-100'>Add to cart</Button>
+          ) : (
+            <>
+              <div
+                className='d-flex align-items-center flex-column'
+                style={{ gap: '0.5rem' }}
+              >
+                <div
+                  className='d-flex align-items-center justify-content-center '
+                  style={{ gap: '0.5rem' }}
+                >
+                  <Button className='btn btn-primary'>-</Button>
+                  <span className='fs-4'>{quantity}</span>
+                  <Button className='btn btn-primary'>+</Button>
+                </div>
+                <Button variant='danger' size='sm'>
+                  Remove
+                </Button>
+              </div>
+            </>
+          )}
+        </div>
       </Card.Body>
     </Card>
   )
